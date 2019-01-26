b39e6a973c86c90b762b3126ae6bc98b62e5c8c0
Kristupas Stumbrys <kristupas.stumbrys@gmail.com>
2019-01-26 19:39:39 +0200

    Adding Message parsing and serializing helpers

--- Assets/Scripts/Network/Messages.cs
+++ Assets/Scripts/Network/Messages.cs
@@ -1,9 +1,37 @@
+using System;
+using UnityEngine;
+
 namespace Messages
 {
 
 [System.Serializable]
 public class Message {
     public string messageType;
+
+    public static void ParseMessage(string json, Action<State> onStateReceived, Action<Tile[]> onBoardReceived) {
+        try
+        {
+            Message message = JsonUtility.FromJson<Message>(json);
+            switch (message.messageType)
+            {
+                case "state":
+                    StateMessage stateMessage = JsonUtility.FromJson<StateMessage>(json);
+                    onStateReceived(stateMessage.state);
+                    break;
+                case "board":
+                    BoardMessage boardMessage = JsonUtility.FromJson<BoardMessage>(json);
+                    onBoardReceived(boardMessage.board);
+                    break;
+                default:
+                    throw new Exception("Unsupported messageType: " + message.messageType);
+            }
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError("Failed to parse: " + json);
+            Debug.LogError(ex.Message);
+        }
+    }
 }
 
 public enum Action {
@@ -21,6 +49,16 @@ public class Intent {
 [System.Serializable]
 public class IntentMessage : Message {
     public Intent intent;
+
+    public static string MakeIntentMessage(Action action, int turnCount) {
+        IntentMessage message = new IntentMessage();
+        message.messageType = "intent";
+        message.intent = new Intent();
+        message.intent.turnCount = turnCount;
+        message.intent.actionId = action;
+
+        return JsonUtility.ToJson(message);
+    }
 }
 
 [System.Serializable]
@@ -51,6 +89,30 @@ public class State {
 [System.Serializable]
 public class StateMessage : Message {
     public State state;
+}
+
+// All possible properties for a tile, since can't have conditional parsing
+[System.Serializable]
+public class TileProperties {
+    public int cost;
+    public string color;
+}
+
+public enum TileType {
+    Street = 0,
+    Empty = 1
+}
+
+[System.Serializable]
+public class Tile {
+    public string type;
+    public TileProperties properties;
+    public TileType typeId;
+}
+
+[System.Serializable]
+public class BoardMessage : Message {
+    public Tile[] board;
 }
 
 }
--- Assets/Scripts/Test/MessageTester.cs
+++ Assets/Scripts/Test/MessageTester.cs
@@ -29,6 +29,12 @@ static class Tester {
         Debug.Log(json);
     }
 
+    [MenuItem("Test/Make Intent Message")]
+    static void MakeIntentMessage() {
+        string json = IntentMessage.MakeIntentMessage(Action.BuyHouse, 4);
+        Debug.Log(json);
+    }
+
     [MenuItem("Test/Parse State Message")]
     static void ParseStateMessage() {
         string filePath = Path.Combine(Application.streamingAssetsPath, "state-message.json");
@@ -39,6 +45,34 @@ static class Tester {
             StateMessage message = JsonUtility.FromJson<StateMessage>(dataAsJson);
             Debug.Log(message.state.players.Length);
             Debug.Log(message.state.me.cash);
+        }
+    }
+
+    [MenuItem("Test/Parse Board Message")]
+    static void ParseBoardMessage() {
+        string filePath = Path.Combine(Application.streamingAssetsPath, "board-message.json");
+        if (File.Exists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+            Debug.Log("json: " + dataAsJson);
+            BoardMessage message = JsonUtility.FromJson<BoardMessage>(dataAsJson);
+            Debug.Log(message.board.Length);
+        }
+    }
+
+    [MenuItem("Test/Handle Board Message")]
+    static void HandleBoardMessage() {
+        string filePath = Path.Combine(Application.streamingAssetsPath, "board-message.json");
+        if (File.Exists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+            Message.ParseMessage(dataAsJson,
+            (State state) => {
+                Debug.Log("Getting state");
+            }, (Tile[] tiles) => {
+                Debug.Log("Getting tiles");
+                Debug.Log(tiles.Length);
+            });
         }
     }
 }
