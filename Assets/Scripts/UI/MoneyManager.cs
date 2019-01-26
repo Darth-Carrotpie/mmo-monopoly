886bf8e8500de2173f2a529de0f5fc97e7966b49
DariusStonys <47043293+DariusStonys@users.noreply.github.com>
2019-01-26 19:28:02 +0200

    UI and Fonts

---
+++ Assets/Scripts/UI/MoneyManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyManager : MonoBehaviour
+{
+    private int playerMoneyChange = 0;
+
+    public Text MoneyTotalCount;
+    public Text MoneyBalanceCount;
+    public Text MoneyTransactionCount;
+
+    void Update()
+    {
+        playerMoneyChange = 100;
+        MoneyTotalCount.GetComponent<Text>().text = "$" + playerMoneyChange.ToString();
+    }
+
+    void updateScore(/*BoardTile[] playerOwnedBoardTiles, BoardTile playerCurrentBoardTile, Player player*/)
+    {
+        /*playerMoneyChange += playerCurrentBoardTile.properties.taxes;
+        MoneyTransactionText.text += "\\n - $" + playerCurrentBoardTile.properties.taxes;
+
+        foreach (BoardTile tile in playerOwnedBoardTiles)
+        {
+            if (tile.properties.taxes != null && tile.properties.taxes != 0)
+            {
+                playerMoneyChange += tile.properties.taxes;
+                MoneyTransactionText.text += "\\n + $" + tile.properties.taxes;
+            }
+        }
+
+        MoneyBalanceCount.text = "$" + playerMoneyChange.ToString();
+        MoneyTotalCount.text = player.Money.ToString();*/
+    }
+}
